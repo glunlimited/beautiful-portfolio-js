38d7ff434ba6e18a9fa6385726c00e4fa28025cd
glunlimited <glunlimited@gmail.com>
2026-07-02 18:50:51 -0400

    Coded background  effect and Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,77 @@
+import { cn } from "@/libs/utils";
+import { Menu, X } from "lucide-react";
+import { useState, useEffect } from "react";
+
+const navItems = [
+    { name: "Home", href: "#home" },
+    { name: "About", href: "#about" },
+    { name: "Skills", href: "#skills" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contact", href: "#contact" },
+];
+
+
+export const NavBar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    
+
+    return (
+        <nav className={cn("fixed w-full z-40 transition-all duration-300",
+            isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+        )}>
+            <div className="container flex justify-between items-center">
+                <a className="text-xl font-bold text-primary flex items-center" href="#hero">
+                    <span className="relative z-10">
+                        <span className="text-glow text-foreground">G & L Unlimited </span> {" "} Portfolio
+                    </span>
+                </a>
+                {/* Desktop Navigation Links */}
+                <div className="hidden md:flex space-x-8">
+                    {navItems.map((item) => (
+                        <a id={item.name + "-link"} href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                        >
+                            {item.name}
+                        </a>
+                    ))}
+                </div>
+
+                {/* Mobile Navigation Links */}
+                <button  onClick={() => setIsMenuOpen((prev) => !prev)}
+                    className="md:hidden p-2 rounded-md text-foreground/80 z-50"
+                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                >
+                    {" "}{isMenuOpen ? <X size={24} /> : <Menu size={24} />}{" "}
+                </button>
+                <div className={cn("fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center", 
+                    "transition-all duration-300 md:hidden", isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+                )}>   
+                    <div className="flex flex-col space-y-8 text-xl">
+                        {navItems.map((item) => (
+                            <a key={item.name} href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                            onClick={() => setIsMenuOpen(false)} // Close menu on link click
+                            >
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+
+            </div> {/*Menu Container End*/}
+            
+        </nav>
+    );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-export const Home = () => {
+import { Star } from "lucide-react";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { StarsBackground } from "@/components/StarsBackground";
+import { NavBar } from "@/components/NavBar";
+
+export const Home = () => { 
+
+
   return (
-    <div>
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden" >
       {/* Theme Toggle */}
+      <ThemeToggle /> 
       {/* Background Effects */}
+      <StarsBackground />
        {/* Navbar*/}
+       <NavBar />
         {/* main content */}
          {/* Footer */}
       <h1>Home</h1>
